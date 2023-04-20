5aba99641397f7f68a53180053e2ddf39c508df0
nick <ndubauskas95@yahoo.com>
2023-04-20 14:28:24 -0700

    logic works. No constraints or swagger still

---
+++ Sorting Hat/View.swift
@@ -0,0 +1,10 @@
+//
+//  View.swift
+//  Sorting Hat
+//
+//  Created by Nick Dubauskas on 4/20/23.
+//
+
+import Foundation
+import UIKit
+let constraint = NSLayoutConstraint(item: displayQuestion, attribute: .top, relatedBy: .equal, toItem: <#T##Any?#>, attribute: .bottom, multiplier: 1.0, constant: 10)
